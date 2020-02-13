9853b9cc430df5c489c19237aea5e967200d7d6a
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-13 17:56:57 -0300

    Updates

---
+++ precache-manifest.5600f1a19e081f7eb8f231e3449f3955.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb2d8ae44f97547f48cc6f59b850bab",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "e672e097a70f4932d22e",
+    "url": "/react-redux/static/js/2.c518929b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-redux/static/js/2.c518929b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5d540191a395747ce3",
+    "url": "/react-redux/static/js/main.ba0aeeda.chunk.js"
+  },
+  {
+    "revision": "ddca964e6544c6c39813",
+    "url": "/react-redux/static/js/runtime-main.10be3f51.js"
+  }
+]);
