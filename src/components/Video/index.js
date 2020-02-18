36a1884068abfb441f972d54e735eb52b24c69f4
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-18 11:08:41 -0300

    refactor(Video): refactoring with useSelector and useDispatch

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-import { connect } from "react-redux";
+import { useSelector } from 'react-redux';
 
-const Video = ({ activeModule, activeLesson }) => (
-  <div>
-    <strong>Módulo {activeModule.title}</strong>
-    <span>Aula {activeLesson.title}</span>
-  </div>
-);
+export default function Video() {
+  const active = useSelector(state => state.course)
 
-export default connect(state => ({
-  activeModule: state.course.activeModule,
-  activeLesson: state.course.activeLesson
-}))(Video);
+  return (
+    <div>
+      <strong>Módulo {active.activeModule.title}</strong>
+      <span>Aula {active.activeLesson.title}</span>
+    </div>
+  );
+};
